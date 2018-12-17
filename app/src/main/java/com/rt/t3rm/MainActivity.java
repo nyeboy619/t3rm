7ba6ee6cb6beb5c5e0b439c2948a597719aeee65
Reneboy Torres <nyeboy619@gmail.com>
2018-12-17 22:10:06 +0800

    Initial commit

---
+++ app/src/main/java/com/rt/t3rm/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rt.t3rm;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
