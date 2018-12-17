6398c3e7a90d9e8b336dc0a9e9c56d3eaff2ca69
Reneboy Torres <nyeboy619@gmail.com>
2018-12-17 23:28:03 +0800

    update class but havent add the activity in manifest file

--- app/src/main/java/com/rt/t3rm/MainActivity.java
+++ app/src/main/java/com/rt/t3rm/MainActivity.java
@@ -1,7 +1,10 @@
 package com.rt.t3rm;
 
 import android.app.*;
+import android.content.*;
 import android.os.*;
+import android.view.*;
+import android.widget.*;
 
 public class MainActivity extends Activity 
 {
@@ -10,5 +13,19 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+				
+				
+				Button signupBtn = findViewById(R.id.signup);
+				
+				
+				
+				
+				
     }
+		
+		
+		public void signUp(View v){
+				Intent i = new Intent(this,Register.class);
+				startActivity(i);
+		}
 }
---
+++ app/src/main/java/com/rt/t3rm/Register.java
@@ -0,0 +1,15 @@
+package com.rt.t3rm;
+import android.app.*;
+import android.os.*;
+
+public class Register extends Activity
+{
+
+		@Override
+		protected void onCreate(Bundle savedInstanceState)
+		{
+				// TODO: Implement this method
+				super.onCreate(savedInstanceState);
+		}
+		
+}
